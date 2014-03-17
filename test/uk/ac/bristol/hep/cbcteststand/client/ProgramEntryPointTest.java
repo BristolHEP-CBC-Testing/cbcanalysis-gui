df8a7ad9ad50df57ccf7147814da188d7ee7d048
Mark Grimes <mark.grimes@bristol.ac.uk>
2014-03-17 11:06:01 +0000

    Changes to the settings to get unit tests working

---
+++ test/uk/ac/bristol/hep/cbcteststand/client/ProgramEntryPointTest.java
@@ -0,0 +1,23 @@
+package uk.ac.bristol.hep.cbcteststand.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class ProgramEntryPointTest extends GWTTestCase
+{
+
+	@Override
+	public String getModuleName()
+	{
+		return "uk.ac.bristol.hep.cbcteststand.CBCTestStandWidgetSet";
+	}
+
+	public void testSimple()
+	{
+		assertTrue( true );
+	}
+
+	public void testSimple2()
+	{
+		assertTrue( true );
+	}
+}
